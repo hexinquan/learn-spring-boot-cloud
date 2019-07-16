0dde9419c4c556c57a681e4bf5b68263ee7a8fac
hexinquan <630050024@qq.com>
2019-07-16 18:17:23 +0800

    修改pom的项目名称

--- learn-boot-config/src/main/java/com/learn/config/ConfigApplication.java
+++ learn-boot-config/src/main/java/com/learn/config/ConfigApplication.java
@@ -4,6 +4,7 @@ import com.learn.config.properties.DataSourceProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
@@ -18,6 +19,7 @@ import org.springframework.core.env.Environment;
  * 4.通过Environment 获取环境配置
  *
  */
+@EnableConfigServer
 public class ConfigApplication {
 
     /**
@@ -36,7 +38,7 @@ public class ConfigApplication {
 
 
         //ConfigurableApplicationContext context = SpringApplication.run(ConfigApplication.class,args);
-        //SpringApplication.run(ConfigApplication.class, args);
+        SpringApplication.run(ConfigApplication.class, args);
 
         //加载多个源
        /* SpringApplication application = new SpringApplication();
@@ -48,8 +50,8 @@ public class ConfigApplication {
          System.out.println(annotationConfigApplicationContext.getBean("user")); // 使用@Component 默认小写类名为key
          System.out.println(annotationConfigApplicationContext.getBean("buildUser")); //使用@Bean  默认为方法的方法名称为key
          **/
-        ConfigurableApplicationContext context = SpringApplication.run(ConfigApplication.class, args);
-        Environment environment = context.getEnvironment(); // 环境应用环境的配置
+       // ConfigurableApplicationContext context = SpringApplication.run(ConfigApplication.class, args);
+      //  Environment environment = context.getEnvironment(); // 环境应用环境的配置
         /**  1.首先会从根目录名为config包下读取对应的application.properties->
           *  2.如果没有再读取名为application.yml->
           *  3.读取根目录非config包以外读取application.properties->
@@ -58,11 +60,11 @@ public class ConfigApplication {
          *   6.要是名称不是appliction.properties 则需要通过spring.config.name (例如:spring.name.config.name=app 直接前缀app就行) 来指定
          *   7.要是名称不是appliction.properties 则需要多个文件或者引用外部文件 则spring.config.location  例如 spring.config.location = classpath:config/app.properties,classpath:config/app1.properties
          **/
-        System.out.println(environment.getProperty("local.ip"));
-        System.out.println(environment.getProperty("local.host"));
+      //  System.out.println(environment.getProperty("local.ip"));
+      //  System.out.println(environment.getProperty("local.host"));
 
-        DataSourceProperties dataSourceConfig = context.getBean(DataSourceProperties.class);
-        System.out.println(dataSourceConfig);
+     //   DataSourceProperties dataSourceConfig = context.getBean(DataSourceProperties.class);
+      //  System.out.println(dataSourceConfig);
 
 
 
