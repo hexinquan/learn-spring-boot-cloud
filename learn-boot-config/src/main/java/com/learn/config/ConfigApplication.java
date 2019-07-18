876e9c653c6e4ec5c2c613a4525e11823626860f
hexinquan <630050024@qq.com>
2019-07-18 16:00:05 +0800

    修改从git拉取配置

--- learn-boot-config/src/main/java/com/learn/config/ConfigApplication.java
+++ learn-boot-config/src/main/java/com/learn/config/ConfigApplication.java
@@ -1,13 +1,8 @@
 package com.learn.config;
 
-import com.learn.config.properties.DataSourceProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 /**
